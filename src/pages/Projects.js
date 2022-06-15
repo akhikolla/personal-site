a69456cce048d40f2f99fca681d832a1e98d5d11
akhikolla <akhilakollasrinu424jf@gmail.com>
2022-06-15 11:57:44 -0400

    on post click

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,6 +7,7 @@ import Cell from '../components/Projects/Cell'
 import data from '../data/projects'
 
 const onProjectClick = (e) => {
+  console.log('e.target value', e.target)
   return <h4>e.target.value</h4>
 }
 
